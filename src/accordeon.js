3a9cd569b1bf24bd8ae3e62e7d37c69223144d7f
Dmitrii Isakov <dimass82@mail.ru>
2021-05-18 15:12:39 +0300

    +accordeon.js

--- src/accordeon.js
+++ src/accordeon.js
@@ -1,18 +1,20 @@
 const featureLinkElems = document.querySelectorAll('.feature__link');
 const featureSubElems = document.querySelectorAll('.feature-sub');
 
-console.log(featureLinkElems);
-console.log(featureSubElems);
 featureLinkElems.forEach((btn, i) => {
   btn.addEventListener('click', () => {
-    
-      featureSubElems.forEach((el) => {
-        el.classList.add('hidden');
+    if (!featureSubElems[i].classList.contains('hidden')) {
+      featureSubElems[i].classList.add('hidden');
+      featureLinkElems[i].classList.remove('feature__link_active');
+    } else {
+      featureSubElems.forEach((featureSubElem) => {
+        featureSubElem.classList.add('hidden');
       });
-    featureLinkElems.forEach((el) => {
-      el.classList.remove('feature__link_active');
-    });
-    featureSubElems[i].classList.remove('hidden');
-    btn.classList.add('feature__link_active');
+      featureLinkElems.forEach((featureLinkElem) => {
+        featureLinkElem.classList.remove('feature__link_active');
+      });
+      featureSubElems[i].classList.remove('hidden');
+      btn.classList.add('feature__link_active');
+    }
   });
 });
